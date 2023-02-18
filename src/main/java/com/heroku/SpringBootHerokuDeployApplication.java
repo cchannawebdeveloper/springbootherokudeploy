84605d54ba62d7092b9052948db1d26d9bb754df
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2023-02-18 19:00:37 +0700

    my first commit

---
+++ src/main/java/com/heroku/SpringBootHerokuDeployApplication.java
@@ -0,0 +1,13 @@
+package com.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHerokuDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHerokuDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heroku/SpringBootHerokuDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHerokuDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
